1269d20eb057c6eb19b8c6d5ccc1f548305e0109
Kostis Karantias <kkarantias@gmail.com>
2018-10-23 21:08:02 +0300

    level: fix case where blockid could be 0

--- src/level.js
+++ src/level.js
@@ -5,11 +5,21 @@ const { shift, compare } = require("math-buffer");
 import type { BlockId } from "./types";
 const { TESTNET_MAX_TARGET } = require("./constants");
 
+const ZERO = Buffer.from([0]);
+
+const equal = (x, y) => compare(x, y) === 0;
+const lessThanOrEqual = (x, y) => compare(x, y) <= 0;
+
 module.exports = function level(blockId: BlockId, target?: BlockId) {
   target = target || TESTNET_MAX_TARGET;
   let tar = Buffer.from(target);
   let shifts = 0;
-  while (compare(blockId, tar) <= 0) {
+
+  if (equal(ZERO, blockId)) {
+    return Infinity;
+  }
+
+  while (lessThanOrEqual(blockId, tar)) {
     shift(tar, -1, tar);
     ++shifts;
   }
--- test/level.test.js
+++ test/level.test.js
@@ -1,9 +1,27 @@
 // @flow
 
+const { fromRev } = require("bcash/lib/utils/util");
+const { fromInt } = require("math-buffer");
 const level = require("../src/level");
 
 describe("level", () => {
   it("returns the level", () => {
     expect(level(Buffer.from([1, 0, 0]), Buffer.from([0, 0, 1]))).toEqual(16);
+  });
+
+  it("works on target and id not being of the same length", () => {
+    expect(level(fromInt(1))).toBeDefined();
+  });
+
+  it("works on zero", () => {
+    expect(level(fromInt(0))).toBeDefined();
+  });
+
+  it("works on an actual testnet block id", () => {
+    const bid = fromRev(
+      "000000000000010cb677cf4c19e403728d3190a083c3086c5a93931ccb78e542"
+    );
+
+    expect(level(bid)).toEqual(23);
   });
 });
